c4978e168683cdb830720a6af9c5a797eacb11e1
NovqiGarrix <novqigarrix@gmail.com>
2024-05-30 12:01:43 +0700

    feat: using new template

--- next.config.js
+++ next.config.js
@@ -1,26 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  eslint: {
-    // Disabling on production builds because we're running checks on PRs via GitHub Actions.
-    ignoreDuringBuilds: true
-  },
+const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
+    domains: ['utfs.io'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/password',
-        destination: '/',
-        permanent: true
-      }
-    ];
-  }
-};
+}
+
+export default nextConfig
